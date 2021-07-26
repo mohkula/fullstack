70bf31322aba2236608a0621be47bbc7b5cadcbe
Mohkula <matias.himanen@helsinki.fi>
2021-07-26 12:26:21 +0300

    tuotantoversio

--- osa3/helloWorld/index.js
+++ osa3/helloWorld/index.js
@@ -1,7 +1,12 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
+
+app.use(cors())
 
 app.use(express.json())
+app.use(express.static('build'))
+
 
 let notes = [
     {
@@ -81,7 +86,7 @@ app.delete('/api/notes/:id', (request, response) => {
     response.json(note)
   })
 
-  const PORT = 3001
+  const PORT = process.env.PORT || 3001
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
   })
