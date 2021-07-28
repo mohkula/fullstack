369e6cb587b2c8986a93b80b681ca272b74114e8
Mohkula <matias.himanen@helsinki.fi>
2021-07-28 13:02:44 +0300

    tehtävä 3.11 build

--- osa2/puhelinluettelo/src/services/Person.js
+++ osa2/puhelinluettelo/src/services/Person.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseUrl = 'https://aqueous-escarpment-93094.herokuapp.com/api/persons'
+const baseUrl = '/api/persons'
 
 const getAll = () => {
     const request = axios.get(baseUrl)
