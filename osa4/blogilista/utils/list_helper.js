e1132dae5219c999095ac70d703bcb5a0a32c0fe
Mohkula <matias.himanen@helsinki.fi>
2021-08-12 12:38:31 +0300

    tehtävä 4.6

--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -21,9 +21,48 @@ const favouriteBlog = (blogs) =>{
 
   return favBlog
 }
+
+const mostBlogs = (blogs) =>{
+  blogs.sort(function(a,b){
+    return a.author.localeCompare(b.author)
+  })
+  
+  var authorWithMostBlogs = blogs[0].author
+  var mB = 0
+  var helper = 0
+  var currentAuthor = blogs[0].author
+  for(var i = 0; i < blogs.length; i++ ){
+    if(blogs[i].author === currentAuthor){
+      helper ++
+//122333
+    }
+    else{
+      
+      if (helper > mB){
+        mB = helper
+        authorWithMostBlogs = currentAuthor
+        
+      }
+      currentAuthor = blogs[i].author
+        helper = 0
+    }
+
+  }
+  helper++
+  
+  if (helper > mB){
+    authorWithMostBlogs = currentAuthor
+
+  }
+
+  return authorWithMostBlogs
+
+  
+}
   
   module.exports = {
     dummy,
     totalLikes,
-    favouriteBlog
+    favouriteBlog,
+    mostBlogs
   }
