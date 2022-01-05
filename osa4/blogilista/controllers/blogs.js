4ee0868a6b2134b7f165fa0e017ba97569c72ea5
Mohkula <matias.himanen@helsinki.fi>
2022-01-05 14:03:11 +0200

    tehtävä 4.17

--- osa4/blogilista/controllers/blogs.js
+++ osa4/blogilista/controllers/blogs.js
@@ -1,11 +1,12 @@
 const blogsRouter = require('express').Router()
 const { urlencoded } = require('express')
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 
 blogsRouter.get('/', async (request, response) => {
 
-  const blogs = await Blog.find({})
+  const blogs = await Blog.find({}).populate('user', {username: 1, name: 1})
   response.json(blogs.map(blog => blog.toJSON()))
   
   })
@@ -22,15 +23,34 @@ blogsRouter.get('/', async (request, response) => {
   
   blogsRouter.post('/', async (request, response) => {
     
-    const blog = new Blog(request.body)
+    const body = request.body
+
+    const users = await User.find({})
+
+    const user = users[0]
+
+
+    const blog = new Blog({
+      title: body.title,
+      author: body.author,
+      url: body.url,
+      likes: body.likes,
+      user: user._id
+
+    })
 
     if(blog.title === undefined || blog.author === undefined){
       return response.status(400).json({ error: 'content missing' })
     }
   
-    await blog.save()
-    response.status(201)
+    const savedBlog = await blog.save()
 
+
+    user.blogs = user.blogs.concat(savedBlog._id)
+ 
+    await user.save()
+
+    response.json(savedBlog.toJSON())
     
       
   })
--- osa4/blogilista/controllers/users.js
+++ osa4/blogilista/controllers/users.js
@@ -6,24 +6,24 @@ usersRouter.post('/', async (request, response) => {
   const body = request.body
 
   if(!body.password){
-    response.status(400).json({error: "No password given"})
+    return response.status(400).json({error: "No password given"})
 
   }
   else if(!body.username ){
-    response.status(400).json({error: "No username given"})
+   return response.status(400).json({error: "No username given"})
 
   }
 
-  else if(body.password.length <3){
-    response.status(400).json({error: "Password too short"})
+  if(body.password.length <3){
+    return response.status(400).json({error: "Password too short"})
 
   }
 
-  else if(body.username.length <3){
+   if(body.username.length <3){
     response.status(400).json({error: "Username too short"})
 
   }
-else{
+
 
 
   const saltRounds = 10
@@ -37,7 +37,7 @@ else{
 
   const savedUser = await user.save()
   .catch(error =>{
-    //console.log(error)
+    console.log(error)
   response.status(400).json({error: error})
 
 })
@@ -45,14 +45,14 @@ else{
 if(savedUser){
   response.json(savedUser)
 }
-}
+
 
   
   
 })
 
 usersRouter.get('/', async (request, response) => {
-    const users = await User.find({})
+    const users = await User.find({}).populate('blogs', {title: 1, author: 1, likes: 1, url: 1})
     response.json(users.map(u => u.toJSON()))
   })
 
