e0dc4bb00b328d1c5aa5974ec05ab76b84af6db2
Mohkula <matias.himanen@helsinki.fi>
2022-01-07 15:00:09 +0200

    tehtävä 4.20

---
+++ osa4/blogilista/utils/middleware.js
@@ -0,0 +1,17 @@
+const { response } = require("express")
+
+const tokenExtractor = (request, response, next) => {
+    const authorization = request.get('authorization')
+    if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+      request.token = authorization.substring(7)
+      
+    }
+    
+
+    next()
+  }
+
+
+  module.exports ={
+      tokenExtractor
+  }
