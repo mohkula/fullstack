932ae31e8a6386a2e50b74437dd87b5d8a7e7126
Mohkula <matias.himanen@helsinki.fi>
2022-01-19 12:59:26 +0200

    tehtävät 5.1 ja 5.2

--- osa5/blog/bloglist-frontend/src/App.js
+++ osa5/blog/bloglist-frontend/src/App.js
@@ -2,8 +2,18 @@ import React, { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
 
+import loginService from './services/login' 
+
+
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [newBlog, setNewBlog] = useState('')
+
+  const [errorMessage, setErrorMessage] = useState(null)
+  const [username, setUsername] = useState('') 
+  const [password, setPassword] = useState('') 
+  const [user, setUser] = useState(null)
+
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -11,12 +21,116 @@ const App = () => {
     )  
   }, [])
 
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogappUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      blogService.setToken(user.token)
+    }
+  }, [])
+
+
+  
+  const handleLogin = async (event) => {
+    event.preventDefault()
+    
+    try {
+      const user = await loginService.login({
+        username, password,
+      })
+
+      window.localStorage.setItem(
+        'loggedBlogappUser', JSON.stringify(user)
+      ) 
+      blogService.setToken(user.token)
+
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch (exception) {
+      setErrorMessage('wrong credentials')
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
+    }
+  }
+
+  const handleLogout = (event) =>{
+    window.localStorage.removeItem('loggedBlogappUser')
+
+  }
+
+
+  const addBlog = (event) =>{
+    event.preventDefault()
+  }
+
+  const handleBlogChange = (event) =>{
+    console.log(event.target.value)
+    setNewBlog(event.target.value)
+  }
+
+  const logoutForm = () => (
+    <div>
+      
+    <button onClick={handleLogout}> Logout</button>
+
+    </div>
+  )
+
+  const loginForm = () => (
+    <form onSubmit={handleLogin}>
+      <div>
+        username
+          <input
+          type="text"
+          value={username}
+          name="Username"
+          onChange={({ target }) => setUsername(target.value)}
+        />
+      </div>
+      <div>
+        password
+          <input
+          type="password"
+          value={password}
+          name="Password"
+          onChange={({ target }) => setPassword(target.value)}
+        />
+      </div>
+      <button type="submit">login</button>
+    </form>      
+  )
+
+  const blogForm = () => (
+    <form onSubmit={addBlog}>
+      <input
+        value={newBlog}
+        onChange={handleBlogChange}
+      />
+      <button type="submit">save</button>
+    </form>  
+  )
+
+
   return (
     <div>
       <h2>blogs</h2>
-      {blogs.map(blog =>
+
+      {user === null ?
+      loginForm() :
+      <div>
+        <p>{user.name} logged in</p> 
+        {logoutForm()}
+        {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
+        {blogForm()}
+      </div>
+    }
+
+    
     </div>
   )
 }
