b2a65c3d6410c4984365964df54e48ccfca001f2
Mohkula <matias.himanen@helsinki.fi>
2022-02-01 11:45:27 +0200

    tehtävät 6.4 ja 6.5

--- osa6/tehtävät/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ osa6/tehtävät/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,13 +1,25 @@
-const anecdotesAtStart = [
+
+
+const getId = () => (100000 * Math.random()).toFixed(0)
+
+const sortAnecdotes = (anecdotes) => {
+
+  anecdotes.sort(function (a, b){
+    return b.votes - a.votes
+  })
+
+  return anecdotes
+
+}
+
+const anecdotesAtStart = sortAnecdotes([
   'If it hurts, do it more often',
   'Adding manpower to a late software project makes it later!',
   'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
   'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
   'Premature optimization is the root of all evil.',
   'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
-]
-
-const getId = () => (100000 * Math.random()).toFixed(0)
+])
 
 const asObject = (anecdote) => {
   return {
@@ -31,9 +43,19 @@ const reducer = (state = initialState, action) => {
         ...anecdoteToVote, 
         votes: anecdoteToVote.votes + 1
       }
-      return state.map(anecdote =>
+
+      
+
+      return sortAnecdotes(state.map(anecdote =>
         anecdote.id !== id ? anecdote : changedAnecdote 
-      )
+      ))
+
+      case 'NEW_ANECDOTE':
+
+      const anecdote = asObject(action.data.content)
+      return sortAnecdotes(state.concat(anecdote))
+
+      
       default:
         return state
   }
@@ -48,6 +70,11 @@ data: {id: id}
 
 }
 
+}
+
+export const addAnecdote = (content) => {
+  return {type: 'NEW_ANECDOTE',
+data:{content: content}}
 }
 
 export default reducer
