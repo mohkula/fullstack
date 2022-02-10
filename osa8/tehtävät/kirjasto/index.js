cc25b61d7ce7ae5d6b8fd2fadbe34c45fc912442
Mohkula <matias.himanen@helsinki.fi>
2022-02-10 12:16:07 +0200

    tehtävä 8.7

--- osa8/tehtävät/kirjasto/index.js
+++ osa8/tehtävät/kirjasto/index.js
@@ -118,6 +118,12 @@ const typeDefs = gql`
       genres: [String]!
      
     ): Book
+
+    editAuthor(
+      name: String!
+      setBornTo: Int!
+
+    ): Author
   }
 
 `
@@ -179,10 +185,17 @@ allAuthors: () => authors
       books = books.concat(book)
 
       return book
-      
+     
+    },
 
-
-
+    editAuthor: (root, args) => {
+      const author = authors.find(a => a.name === args.name)
+      if(!author){
+        return null
+      }
+      const updatedAuthor = {...author, born: args.setBornTo}
+      authors = authors.map(a => a.name === args.name ? updatedAuthor: a)
+      return updatedAuthor
 
     }
 
